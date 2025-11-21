a29dd884047b86fa79ce4dc53310f25eeede4e87
Chaithanya-ks <chaithanyakedilaya@gmail.com>
2025-11-22 00:07:30 +0530

    first commit

---
+++ lms project/server/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js';
+import { cllerkWebhooks } from './controllers/webhooks.js';
+
+//initialize express
+const app = express();
+
+//connect to database
+await connectDB();
+
+//add middleware
+app.use(cors());
+
+//routes
+app.get('/',(req,res)=>{
+    res.send("api working...");
+})
+app.post('/clerk',express.json(), cllerkWebhooks)
+
+
+
+
+
+//port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`server is running at port ${PORT}`);
+})
